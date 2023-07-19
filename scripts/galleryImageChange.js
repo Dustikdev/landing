5a8759df9ef454ba2a1898c72677f3d9a7a5b52c
Никита Швец <dustikappple@icloud.com>
2023-07-19 20:39:52 +0700

    gallery js update

---
+++ scripts/galleryImageChange.js
@@ -0,0 +1,23 @@
+﻿"use strict"
+
+    var imageLens = document.querySelectorAll('.image-lens');
+    imageLens.forEach(function(imageLens) {
+        const selectedImg = imageLens.querySelector('img');
+        imageLens.addEventListener('mouseenter', function() {
+        const lens = document.createElement('img');
+        lens.src = './assets/galleryPhotos/lens.svg';
+        lens.classList.add('lens');
+        lens.style.position = 'absolute';
+        lens.style.left = '42%';
+        lens.style.top = '41%';
+        lens.style.opacity = '1';
+        imageLens.appendChild(lens);
+        selectedImg.style.opacity = '0.1';
+      });
+      selectedImg.addEventListener('mouseleave', function() {
+        const lens = document.querySelector('.lens');
+        lens.style.opacity = '0';
+        lens.remove();
+        selectedImg.style.opacity = '1';
+      });
+    });
