a69a2e41930530bcce2d6748055ced56c1a98380
Никита Швец <dustikappple@icloud.com>
2023-07-21 21:12:56 +0700

    header-mobile-commit

---
+++ scripts/navMenuTransform.js
@@ -0,0 +1,18 @@
+﻿var element = document.querySelector(".icon-button");
+
+element.addEventListener("click", function() {
+
+    var header = document.querySelector("header");
+    var nav = header.querySelector('nav');
+    nav.style.display = 'flex';
+    setTimeout(function() {
+        nav.classList.add("visible");
+      }, 1); 
+    
+
+    // alert('skdjfhsjdkf')
+
+
+
+
+});
