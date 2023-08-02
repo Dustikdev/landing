29a3e6015d2854d790a9a5fea7768475150ada99
Никита Швец <dustikappple@icloud.com>
2023-08-02 14:55:49 +0700

    reworking toggle script

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+﻿const pressedTab = document.querySelector(".pressed-tab");
+const unpressedTab = document.querySelector(".unpressed-tab");
+const presentationElement = document.querySelector("#presentation");
+const imagesElements = presentationElement.querySelectorAll(".images");
+
+function pressTab(tabs) {
+  tabs.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      if (elem.classList.contains("unpressed-tab")) {
+        for (item of tabs) {
+          item.classList.add("unpressed-tab");
+          item.classList.remove("pressed-tab");
+        };
+        elem.classList.add("pressed-tab");
+        elem.classList.remove("unpressed-tab");
+        for (item of imagesElements) {
+          item.classList.toggle("invisible-images");
+          item.classList.toggle("visible-images");
+        };
+      }
+    });
+  });
+}
+pressTab([pressedTab, unpressedTab]);
