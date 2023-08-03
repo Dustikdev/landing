60963dec2e4a9bce991ddaaa93c5b9d714083af1
Никита Швец <dustikappple@icloud.com>
2023-08-03 22:46:40 +0700

    galery fixes

---
+++ scripts/galleryScripts.js
@@ -0,0 +1,31 @@
+﻿//скрипт для заблюривания фона и добавления линзы
+function blur() {
+  const gallery = document.querySelector(".gallery-content");
+  gallery.addEventListener("mouseover", function (event) {
+    const image = event.target;
+    if (image.classList.contains("selected-img")) {
+      image.classList.add("blurred");
+      const markup = `<img src="./assets/galleryPhotos/lens.svg" class="lens"/>`;
+      image.parentNode.insertAdjacentHTML("beforeend", markup);
+    }
+  });
+  gallery.addEventListener("mouseout", function (event) {
+    const image = event.target;
+    if (image.classList.contains("selected-img")) {
+      image.classList.remove("blurred");
+      image.parentNode.querySelector(".lens").remove();
+    }
+  });
+  gallery.addEventListener("click", function (event) {
+    const image = event.target;
+    const modalWindow = document.querySelector(".modal-window");
+    if (image.classList.contains("selected-img")) {
+      let modalImage = event.target.cloneNode(true);
+      modalImage.removeAttribute("class");
+      console.log(modalImage);
+      modalWindow.classList.add("visible");
+      modalWindow.append(modalImage);
+    }
+  });
+}
+blur();
